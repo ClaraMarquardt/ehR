edb296b5f7d54503118e14e7ac270f906215b48c
cz425 <marquardt.clara@gmail.com>
2017-04-13 18:37:02 -0500

    cm 12/04/2017 improve one_hot_encoding

--- R/one_hot_encoding.R
+++ R/one_hot_encoding.R
@@ -12,44 +12,52 @@
 #' @return Modified data.table.
 #' @examples
 
-one_hot_encoding <- function(dt, var_list, drop=FALSE) {
+ one_hot_encoding <- function (dt, var_list, drop = FALSE) {
 
-  contr.onehot = function (n, contrasts, sparse = FALSE) {
-    contr.sum(n = n, contrasts = FALSE, sparse = sparse)
-  }
-
-  current.na.action <- options('na.action')
-
-  options(contrasts = c("contr.onehot", "contr.onehot"))
-  options(na.action='na.pass')
-
-  dt_factor <- lapply(var_list, function(x) {
-  
-    dt_factor_temp <- data.frame(model.matrix(~ . -1, data = dt[, mget(x)]))
-
-    if (drop==TRUE) {
-      dt_factor_temp[[names(dt_factor_temp)[1]]] <- NULL
+    contr.onehot = function(n, contrasts, sparse = FALSE) {
+        contr.sum(n = n, contrasts = FALSE, sparse = sparse)
     }
+    current.na.action <- options("na.action")
+    options(contrasts = c("contr.onehot", "contr.onehot"))
+    options(na.action = "na.pass")
 
-    return(dt_factor_temp)
+    dt_factor <- dt[, c(var_list), with=F]
+    dt_factor <- lapply(var_list, function(x) {
+        dt_factor_temp <- data.frame(model.matrix(~. - 1, data = dt_factor[,
+            mget(x)]))
 
-  })
+        # drop one if only two columns
+        if (ncol(dt_factor_temp)==2 & names(dt_factor_temp)[1] %like% "(0|1)$") {
+            name_orig <- gsub("(0|1)$", "", names(dt_factor_temp)[1])
+            dt_factor_temp <- dt_factor_temp[, 1]
+            dt_factor_temp <- data.frame(dt_factor_temp)
+            names(dt_factor_temp) <- name_orig
+        }
 
-  dt_factor <- data.frame(dt_factor)
-  dt_factor <- as.data.table(dt_factor)
- 
-  # format
-  dt_factor[, names(dt_factor):=lapply(.SD, function(x) as.integer(x))]
+        # drop one column - avoid colinearity
+        if (drop == TRUE & ncol(dt_factor_temp)>1) {
+            dt_factor_temp[[names(dt_factor_temp)[1]]] <- NULL
+        }
+        return(dt_factor_temp)
+    })
 
-  # combine with other data
-  dt_non_factor <- dt[, mget(setdiff(names(dt), var_list))]
-  dt_temp <- data.table(data.frame(dt_non_factor,dt_factor ))
- 
-  options(na.action=current.na.action)
+    dt_factor <- data.frame(dt_factor)
+    dt_factor <- as.data.table(dt_factor)
 
-  # return
-  return(dt_temp)
+    # format
+    dt_factor[, `:=`(names(dt_factor), lapply(.SD, function(x) as.integer(x)))]
 
+    # check that row count aligns
+    if (nrow(dt_factor) != nrow(dt_non_factor)) 
+      print("warning - one hot encoding () - it appears that rows are dropped during the conversion")
+
+    dt_non_factor <- dt[, c(setdiff(names(dt), var_list)), with=F]
+    dt_temp <- data.table(data.frame(dt_non_factor, dt_factor))
+
+    options(na.action = current.na.action)
+
+    # return
+    return(dt_temp)
 }
 
 #----------------------------------------------------------------------------#
