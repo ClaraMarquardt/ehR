cb81ad889d2cefca45310f00fec8e1cc8c9e728e
cz425 <marquardt.clara@gmail.com>
2017-05-09 23:43:42 -0500

    cm 09/04/2017 add exist_ function

--- R/exist_add.R
+++ R/exist_add.R
@@ -2,13 +2,15 @@
 
 #' Return object/object returned by expression if the object exists, return NULL otherwise. 
 #' 
+#' @description 
+#' 
 #' @export
 #' @param TBC 
 #' @return TBC
 #' @examples
 #' TBC
 
-exist_ <- function(obj_exp) {
+exist_add <- function(obj_exp) {
 
   temp <- tryCatch(eval(parse(text=obj_exp)), error=function(e) return(NULL))
 
