a3df4b3468013c7b5dd4addcecfe5a0e882a90e1
cz425 <marquardt.clara@gmail.com>
2017-08-24 18:02:33 -0500

    cm 24/08/2017 delete non ehR-relevant functions

--- github_backup.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/claramarquardt/Google_Drive/Jobs/zolab/mulab/other_project/method_new_repo/ehR
-/usr/bin/git add . -u
-/usr/bin/git add . -A
-/usr/bin/git commit -a -m "cm `date` daily ehR backup (origin: local)"
-/usr/bin/git push backup master:ehR
